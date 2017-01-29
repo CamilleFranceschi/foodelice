dfac04741d42c7d6510e37cd1c4f4325793b97c1
CamilleFranceschi <franceschi.camille@gmail.com>
2017-01-29 15:44:26 +0100

    mailto & outline none on images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 function initMap() {
-        var uluru = {lat: 48.847539 , lng: 2.3277};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+  var uluru = {lat: 48.847539 , lng: 2.3277};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
